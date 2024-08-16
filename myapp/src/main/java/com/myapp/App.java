e779d732b3f8f67ebe949ba032299ec30f300096
Omer <omer94@gmail.com>
2024-08-16 16:07:35 +0300

    checking if version number after changing values in app.java

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Omer Cohen is the new 'Hello world!' Message" );
+        System.out.println( "this is myapp version 1.0.2!" );
     }
 }
