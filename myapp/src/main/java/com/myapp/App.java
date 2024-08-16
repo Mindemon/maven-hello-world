f959aec1da390db206c32efb849fe5c48a0958e2
Omer <omer94@gmail.com>
2024-08-16 22:24:07 +0300

    modifed app.java to test directory check

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World From Omer! V2!" );
+        System.out.println( "this print command was altred!" );
     }
 }
