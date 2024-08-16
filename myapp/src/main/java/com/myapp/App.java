e9f8c6105a7a0ebbfbf53ad8e60bb9fa506dc491
Omer <omer94@gmail.com>
2024-08-16 17:10:37 +0300

    added tag trigger on yml file and change version again

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is myapp version 1.0.2 testing this!" );
+        System.out.println( "this is myapp version 1.0.3 testing this!" );
     }
 }
