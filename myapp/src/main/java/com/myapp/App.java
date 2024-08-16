b3c0b2a9c38755d26c7e1a4412f57493288cc0bd
Omer <omer94@gmail.com>
2024-08-16 17:29:59 +0300

    updating java file to see if it upgrades to 1.0.1

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is myapp version 1.0.0 testing this!" );
+        System.out.println( "this build should be  on version 1.0.1" );
     }
 }
