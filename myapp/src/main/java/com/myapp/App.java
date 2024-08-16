f3330cf5a60b54a239e0dbacb6e6a75ef56a8a1a
Omer <omer94@gmail.com>
2024-08-16 17:05:27 +0300

    changed app.java to test

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is myapp version 1.0.0" );
+        System.out.println( "this is myapp version 1.0.0 testing this!" );
     }
 }
