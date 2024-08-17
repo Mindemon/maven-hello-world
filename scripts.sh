3a908da45ed95037ddecf5eb44ecc71290975d64
Omer <omer94@gmail.com>
2024-08-17 14:24:42 +0300

    fixed branch grep rule

--- scripts.sh
+++ scripts.sh
@@ -22,7 +22,7 @@ determine_version() {
     fi
   elif [[ $GITHUB_REF == refs/heads/feature/* ]]; then
     # Find the latest release version
-  LATEST_RELEASE=$(git branch -r | grep 'origin/feature/.*-1\.0\.x' | sed 's|origin/feature/||' | sort -V | tail -n 1 | xargs echo -n)
+  LATEST_RELEASE=$(git branch -r | grep 'origin/feature/.*-1\.0\..*' | sed 's|origin/feature/||' | sort -V | tail -n 1 | xargs echo -n)
   if [[ -n $LATEST_RELEASE ]]; then
     VERSION="$LATEST_RELEASE-SNPASHOT.jar"
   else
