b3d6e2f0c3965e27097f1d94b5babd1d1ad91a44
Omer <omer94@gmail.com>
2024-08-17 14:41:30 +0300

    calling for the roflcopter

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,13 +1,56 @@
 package com.myapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World STAGING version 1.0.1 REV1" );
-    }
+public class app {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (true) {
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("         :LOL:ROFL:ROFL");
+			System.out.println("           A           ");
+			System.out.println("       /--------       ");
+			System.out.println(" LOL====      ()\\     ");
+			System.out.println("  L    \\         \\        ");
+			System.out.println("        \\_________]                ");
+			System.out.println("           I     I      ");
+			System.out.println("        -----------/              ");
+			System.out.println("                       ");
+			System.out.println("      Alex's ROFLCOPTER!              ");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("ROFL:ROFL:LOL:         ");
+			System.out.println("           A           ");
+			System.out.println("  L    /--------       ");
+			System.out.println(" LOL====      ()\\     ");
+			System.out.println("       \\         \\        ");
+			System.out.println("        \\_________]                ");
+			System.out.println("           I     I      ");
+			System.out.println("        -----------/              ");
+			System.out.println("                       ");
+			System.out.println("      Alex's ROFLCOPTER!              ");
+
+			Thread.sleep(100);
+		}
+
+	}
+
 }
