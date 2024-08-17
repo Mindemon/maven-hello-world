096cb0e571cd0f71ddb4f5d18936e17139ca8a4b
Omer <omer94@gmail.com>
2024-08-17 17:56:47 +0300

    Revert "added timeout for 5 seconds so github wont ban me for making an infinte loop in build"
    This reverts commit a3cde8633e581a44d05727d66d60b9d8b70d5084.

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,5 +1,6 @@
 package com.myapp;
 
+<<<<<<< HEAD
 /**
  * Hello world!
  *
@@ -11,3 +12,59 @@ public class App
         System.out.println( "Hello World STAGING version 1.0.1" );
     }
 }
+=======
+public class App {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (true) {
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("         :LOL:ROFL:ROFL");
+			System.out.println("           A           ");
+			System.out.println("       /--------       ");
+			System.out.println(" LOL====      ()\\     ");
+			System.out.println("  L    \\         \\        ");
+			System.out.println("        \\_________]                ");
+			System.out.println("           I     I      ");
+			System.out.println("        -----------/              ");
+			System.out.println("                       ");
+			System.out.println("     GET TO THE CHOPPA!!!              ");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("ROFL:ROFL:LOL:         ");
+			System.out.println("           A           ");
+			System.out.println("  L    /--------       ");
+			System.out.println(" LOL====      ()\\     ");
+			System.out.println("       \\         \\        ");
+			System.out.println("        \\_________]                ");
+			System.out.println("           I     I      ");
+			System.out.println("        -----------/              ");
+			System.out.println("                       ");
+			System.out.println("      RESUCE IS HERE!!!              ");
+
+			Thread.sleep(100);
+		}
+
+	}
+
+}
+>>>>>>> parent of a3cde86 (added timeout for 5 seconds so github wont ban me for making an infinte loop in build)
